ad308a79722c2e7508d0b5408c66287702d53b47
geweitong <13898712211@163.com>
2020-06-19 17:52:42 +0800

    feat: 提交新模版demo

--- app/panel.js
+++ app/panel.js
@@ -1,14 +1,10 @@
-const panelRule = require.context('./', true, /^\.\/(plugins|views)(\/[\w.-]+)?\/.arena.jsx$/);
-
+const panelRule = require.context('./', true, /^\.\/(plugins|views)(\/[\w.-]+)?\/(.+).arena.jsx$/);
 let panels = {};
 
 panelRule.keys().forEach(key => {
-  const target = pluginRule(key).default;
-  panels = Object.assign({}, panels, { [target.name]: function() { return new target().render() }});
-
+  const target = panelRule(key).default;
+  panels = Object.assign({}, panels, {[target.name]: function() { return new target().render() }});
 });
 
 export default panels;
-
-
 
